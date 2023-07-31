830c83ea47de1c89d72f471977c26411dea85b84
20181014050009 <r.monteiro@escolar.ifrn.edu.br>
2023-07-31 16:45:46 -0300

    inicial

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# proj-asa
-Projeto de teste
+# Projeto Teste
---
+++ email/users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Senha para usuário camila"
+adduser -q --gecos " " camila
+
+echo "Senha para usuário taylor"
+adduser -q --gecos " " taylor
